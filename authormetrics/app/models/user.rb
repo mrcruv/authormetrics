4a3ca4eaaf4e30a79683eb514a23573f8c98f000
tiziano finizzi <finizzi.1915610@studenti.uniroma1.it>
2022-07-31 14:31:13 +0200

    new relations models

--- authormetrics/app/models/user.rb
+++ authormetrics/app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
     has_many :review
     has_many :author_rating
     has_many :publication_rating
+
+    has_many :publication, :through => :favorite_publication
+    has_many :publication, :through => :comment
+    has_many :publication. :through => :publication_rating
+
+    has_many :author, :through => :favorite_author
+    has_many :author, :through => :author_rating
+    has_many :author, :through => :review
+
+
 end
