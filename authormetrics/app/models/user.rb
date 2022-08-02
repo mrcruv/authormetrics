2077d6df41c08d3eb8ac492b79faf7d37f468f99
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-08-02 15:18:21 +0200

    Update user model and view
    aggiunto supporto Devise

--- authormetrics/app/models/user.rb
+++ authormetrics/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     #has_one :banned_user
     has_many :favorite_author, dependent: :destroy
     has_many :favorite_publication,dependent: :destroy
@@ -21,7 +25,7 @@ class User < ApplicationRecord
     reg_date = Date.today
 
     validates :username, :presence=>true, uniqueness: { case_sensitive: true }
-    validates :password, :presence=>true, length: { in: 12..20 }
+    validates :password, :presence=>true, length: { in: 12..255 }
     validates :name, :presence=>true,length: { in: 2..50 }
     validates :surname, :presence=>true,length: { in: 2..50 }
     validates :birth_date, :presence=>true, comparison:  {less_than_or_equal_to: Date.today}
