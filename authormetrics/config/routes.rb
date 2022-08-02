2be9421ccbdd85115341182c5a6ca6e95f25e4dc
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-08-02 15:20:06 +0200

    Update routes.rb
    modifica routes per utenti autenticati/non autenticati

--- authormetrics/config/routes.rb
+++ authormetrics/config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
- 
-  resources :users do 
-    resources :favorite_publications
-    resources :favorite_authors
-    resources :reviews
+  devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'users#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => 'devise/registrations#new', as: :unauthenticated_root
+    end
+    get '/users/sign_out' => 'devise/sessions#destroy'
   end
+
+  # resources :users do 
+  #   resources :favorite_publications
+  #   resources :favorite_authors
+  #   resources :reviews
+  # end
   resources :authors do
     resources :publications
     resources :author_ratings
@@ -18,15 +29,8 @@ Rails.application.routes.draw do
   resources :banned_users
   resources :bans
   
-  
-  
-  
-  
-  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  root "users#new"
   get "/users", to: "users#index"
   get "/users/:id", to: "users#show"
 
