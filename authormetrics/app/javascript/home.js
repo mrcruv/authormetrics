04edcdc01abbe1684e2dd1a81d082e4e814237f5
sarattiani <saraatt@LAPTOP-2DB2K8U0.localdomain>
2022-08-11 18:54:42 +0200

    home4

---
+++ authormetrics/app/javascript/home.js
@@ -0,0 +1,29 @@
+
+function createSearchBar(placeholder){
+    var search_box= document.createElement("div");
+    search_box.setAttribute("class", "search-box"); 
+    var p1=document.getElementById("p1");
+    p1.replaceChild(search_box, p1.childNodes[0]);
+    var bar= document.createElement("input"); 
+    bar.setAttribute("type", "text"); 
+    bar.setAttribute("class", "search"); 
+    bar.setAttribute("placeholder", placeholder);
+    search_box.appendChild(bar);
+    var button= document.createElement("button"); 
+    search_box.appendChild(button);
+    var icon= document.createElement("i");
+    icon.setAttribute("class","fa-solid fa-magnifying-glass");
+    button.appendChild(icon);
+}
+
+function searchByAuth(){ 
+    document.getElementById("ByAuth").style.backgroundColor = "#90caf9";
+    document.getElementById("ByPub").style.backgroundColor = "";
+    createSearchBar("Search by auth...");
+}
+
+function searchByPub(){    
+    document.getElementById("ByPub").style.backgroundColor = "#90caf9";
+    document.getElementById("ByAuth").style.backgroundColor = "";
+    createSearchBar("Search by pub...");
+}
