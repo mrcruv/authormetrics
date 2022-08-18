fe90367c076544b6c888888764f4669efe9436d0
tiziano finizzi <finizzi.1915610@studenti.uniroma1.it>
2022-08-18 17:14:48 +0200

    search v1

--- authormetrics/app/javascript/home.js
+++ authormetrics/app/javascript/home.js
@@ -1,3 +1,5 @@
+
+
 function createSearchBar(placeholder){
     var search_box= document.createElement("div");
     search_box.setAttribute("class", "search_box"); 
@@ -11,8 +13,10 @@ function createSearchBar(placeholder){
     search_box.appendChild(bar);
     var button= document.createElement("button");
     button.setAttribute("class", "search_button");
-    if (placeholder=="Search by auth..."){button.addEventListener("click",function(){search_by_auth();});}
-    else {button.addEventListener("click", function(){ search_by_pub();}); }
+    var text= "http://127.0.0.1:3000/authors/search/"+
+    button.setAttribute("onclik", `location.href='${s}'`);
+    //if (placeholder=="Search by auth..."){button.addEventListener("click",function(){search_by_auth();});}
+    //else {button.addEventListener("click", function(){ search_by_pub();}); }
     search_box.appendChild(button);
     var icon= document.createElement("i");
     icon.setAttribute("class","fa-solid fa-magnifying-glass");
@@ -34,19 +38,19 @@ function searchByPub(){
     }
     createSearchBar("Search by pub...");
 }
-
+/*
 function search_by_auth(){
     text= document.getElementById("search_text_input").value;
-    alert(text);
-    const http = new XMLHttpRequest();
-    const url='http://127.0.0.1:3000/';
+    const url='http://127.0.0.1:3000/authors/search/'+text;
+    const http=new XMLHttpRequest();
     http.open("GET", url);
     http.send();
     http.onreadystatechange = (e) => {
-        console.log(Http.responseText) 
+        console.log(http.responseText) 
     }
+    alert(http.responseText);
 }
-
+*/
 function search_by_pub(){
     text= document.getElementById("search_text_input").value;
     alert(text);
