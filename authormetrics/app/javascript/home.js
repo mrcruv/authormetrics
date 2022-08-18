e82528da610e36ec5a3f2552a7d01b459719f93d
sarattiani <saraatt@LAPTOP-2DB2K8U0.localdomain>
2022-08-18 17:47:12 +0200

    search bar solved

--- authormetrics/app/javascript/home.js
+++ authormetrics/app/javascript/home.js
@@ -1,5 +1,3 @@
-
-
 function createSearchBar(placeholder){
     var search_box= document.createElement("div");
     search_box.setAttribute("class", "search_box"); 
@@ -13,10 +11,8 @@ function createSearchBar(placeholder){
     search_box.appendChild(bar);
     var button= document.createElement("button");
     button.setAttribute("class", "search_button");
-    var text= "http://127.0.0.1:3000/authors/search/"+
-    button.setAttribute("onclik", `location.href='${s}'`);
-    //if (placeholder=="Search by auth..."){button.addEventListener("click",function(){search_by_auth();});}
-    //else {button.addEventListener("click", function(){ search_by_pub();}); }
+    if (placeholder=="Search by auth..."){button.addEventListener("click",function(){search_by_auth();});}
+    else {button.addEventListener("click", function(){ search_by_pub();}); }
     search_box.appendChild(button);
     var icon= document.createElement("i");
     icon.setAttribute("class","fa-solid fa-magnifying-glass");
@@ -38,37 +34,15 @@ function searchByPub(){
     }
     createSearchBar("Search by pub...");
 }
-/*
+
 function search_by_auth(){
     text= document.getElementById("search_text_input").value;
-    const url='http://127.0.0.1:3000/authors/search/'+text;
-    const http=new XMLHttpRequest();
-    http.open("GET", url);
-    http.send();
-    http.onreadystatechange = (e) => {
-        console.log(http.responseText) 
-    }
-    alert(http.responseText);
+    const url="http://127.0.0.1:3000/authors/search/"+text;
+    window.location.href = url;
 }
-*/
+
 function search_by_pub(){
     text= document.getElementById("search_text_input").value;
-    alert(text);
+    const url="http://127.0.0.1:3000/publications/search/"+text;
+    window.location.href = url;
 }
-
-
-
-
-
-    /*
-function provaPub(){
-
-    const searchInput = document.getElementById("ByPub");
-    const s=createSearchBar("Search by pub...");
-    // listen for user events
-    s.addEventListener("keyup", (event) => {
-        const { value } = event.target;
-        window.location.replace("'http://127.0.0.1:3000/publication/search/'");
-    });
-}
-*/
