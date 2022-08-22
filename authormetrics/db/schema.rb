daba1becc672227a0242aa6cb076d1aa9bbd226a
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-08-22 14:23:04 +0200

    Update schema.rb
    update schema

--- authormetrics/db/schema.rb
+++ authormetrics/db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_14_120149) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_22_073923) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "administrators", primary_key: "admin_id", id: :serial, force: :cascade do |t|
     t.string "username", limit: 255, null: false
-    t.string "password", limit: 255, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_administrators_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_administrators_on_reset_password_token", unique: true
     t.index ["username"], name: "administrators_username_key", unique: true
   end
 
