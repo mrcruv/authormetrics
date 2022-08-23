4de2b4385452148a895ec47da27b5c3a8974ab92
tiziano finizzi <finizzi.1915610@studenti.uniroma1.it>
2022-08-23 14:35:48 +0200

    js search pub by author

--- authormetrics/app/javascript/home.js
+++ authormetrics/app/javascript/home.js
@@ -90,3 +90,39 @@ function most_cited_publication_graph(){
     alert(actual_chart);
     document.getElementById(actual_chart).style.visibility= "visible";
 }
+
+//////////////////////////////////////////////////////////////////////////
+
+function createSearchBar2(placeholder,id){
+    var search_box= document.createElement("div");
+    search_box.setAttribute("class", "search_box"); 
+    var p1=document.getElementById("p1");
+    p1.replaceChild(search_box, p1.childNodes[0]);
+    var bar= document.createElement("input"); 
+    bar.setAttribute("type", "text"); 
+    bar.setAttribute("class", "search_text_input");
+    bar.setAttribute("id", "search_text_input") 
+    bar.setAttribute("placeholder", placeholder);
+    search_box.appendChild(bar);
+    var button= document.createElement("button");
+    button.setAttribute("class", "search_button");
+    button.addEventListener("click",function(){search_pub_by_auth(id);});
+    search_box.appendChild(button);
+    var icon= document.createElement("i");
+    icon.setAttribute("class","fa-solid fa-magnifying-glass");
+    button.appendChild(icon);
+}
+
+function searchAuthByPub(id){    
+    document.getElementById("AuthByPub").style.backgroundColor = "#9ccc65";
+    //if (document.getElementById("ByAuth")){
+    //    document.getElementById("ByAuth").style.backgroundColor = "";
+    //}
+    createSearchBar2("Search Auth by Pub...",id);
+}
+
+function search_pub_by_auth(id){
+    text= document.getElementById("search_text_input").value;
+    const url="http://127.0.0.1:3000/authors/"+id+"/search_pub_by_auth/"+text;
+    window.location.href = url;
+}
