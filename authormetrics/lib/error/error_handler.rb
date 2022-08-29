a31391c42cd2d8a52aab3a2b9c3e0119e67b4d93
tiziano finizzi <finizzi.1915610@studenti.uniroma1.it>
2022-08-29 17:23:22 +0200

    error_handler

--- authormetrics/lib/error/error_handler.rb
+++ authormetrics/lib/error/error_handler.rb
@@ -1,18 +1,37 @@
 module Error
-    module ErrorHandler
-      def self.included(clazz)
-        clazz.class_eval do
-          rescue_from CustomError do |e|
-            respond(e.error, e.status, e.message)
-          end
+  module ErrorHandling
+    def self.included(klass)
+      klass.class_eval do
+        rescue_from ActiveRecord::RecordNotFound do |e|
+          respond(:record_not_found, 404, e.to_s)
         end
-      end
-  
-      private
-  
-      def respond(_error, _status, _message)
-        json = Helpers::Render.json(_error, _status, _message)
-        render json: json
+        rescue_from ActiveRecord::ActiveRecordError do |e|
+          respond(e.error, 422, e.to_s)
+        end
+        rescue_from ActiveController::ParameterMissing do |e|
+          response(:unprocessable_entitry, 422, e.to_s)
+        end
+        rescue_from ActiveModel::ValidationError do |e|
+          response(e.error, 422, e.to_s)
+        end
+        rescue_from CustomApiError do |e|
+          respond(e.error, e.status, e.message.to_s)
+        end
+        rescue_from CanCan::AccessDenied do
+          respond(:forbidden, 401, "current user isn't authorized for that")
+        end
+        rescue_from StandardError do |e|
+          respond(:standard_error, 500, e.to_s)
+        end
       end
     end
   end
+  
+    private
+
+    def respond(_error, _status, _message)
+      json = Helpers::Render.json(_error, _status, _message)
+      render json: json
+    end
+  end
+end
---
+++ authormetrics/lib/error/not_visible_error.rb
@@ -0,0 +1,8 @@
+
+module Error
+    class NotVisibleError < CustomError
+      def initialize
+        super(:you_cant_see_me, 422, 'You can\'t see me')
+      end
+    end
+  end
