99fa044f92ef874e8022f5fe8a28f5e8b22d5c61
sarattiani <saraatt@LAPTOP-2DB2K8U0.localdomain>
2022-08-30 00:40:24 +0200

    last standard charts

--- authormetrics/app/controllers/home_controller.rb
+++ authormetrics/app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
     @most_cited_pubs= Publication.distinct.order(cited_by: :desc).where.not(cited_by:nil).first(5)
     @most_cited_authors= CitedBy.order(all_citations: :desc).where.not(all_citations:nil).first(5)
     @most_reviewed_by_authors=Review.group(:author_id).count.first(10).sort_by{|x,y| -y}
+    @most_commented_pubs=Comment.group(:publication_id).count.first(10).sort_by{|x,y| -y}
   end
 
 end
--- authormetrics/app/services/graphs.rb
+++ authormetrics/app/services/graphs.rb
@@ -25,11 +25,37 @@ class Graphs
         return authors_avg_ratings.sort_by {|k, v| -v}.first(top_n).to_h
     end
 
+    def pubs_avg_ratings(pubs, top_n)
+      pubs_avg_ratings={}
+      pubs.each do |pub|
+        ratings_sum=0
+        n= pub.publication_rating.length
+        pub.publication_rating.each do |rating|
+          ratings_sum += rating.rating
+        end
+        if n>0
+          pubs_avg_ratings[pub.title]= ratings_sum.to_f/n
+        else
+          pubs_avg_ratings[pub.title]= 0
+        end
+      end 
+      return pubs_avg_ratings.sort_by {|k, v| -v}.first(top_n).to_h
+  end
+
     def most_reviewed_by_authors_graph(authors)
       h={}
       authors.each do |author|
         author_name=Author.find(author[0]).name
         h[author_name]=author[1]
+      end
+      return h
+    end
+
+    def most_commented_pubs_graph(pubs)
+      h={}
+      pubs.each do |pub|
+        pub_title=Publication.find(pub[0]).title
+        h[pub_title]=pub[1]
       end
       return h
     end
