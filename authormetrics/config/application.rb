330f2a1b1ac0d09ab5349a56636165b9d1d24623
tiziano finizzi <finizzi.1915610@studenti.uniroma1.it>
2022-08-29 22:41:53 +0200

    ssl upgrade

--- authormetrics/config/application.rb
+++ authormetrics/config/application.rb
@@ -25,6 +25,6 @@ module Authormetrics
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # config.force_ssl = true
+    config.force_ssl = true
   end
 end
