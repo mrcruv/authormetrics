4769d8ea1ea12d78e1bf59faa7e2752dd86e87bb
tiziano finizzi <finizzi.1915610@studenti.uniroma1.it>
2022-08-31 17:12:50 +0200

    error handling v2 applicationController

--- authormetrics/app/controllers/application_controller.rb
+++ authormetrics/app/controllers/application_controller.rb
@@ -1,17 +1,40 @@
 class ApplicationController < ActionController::Base
-  
   protect_from_forgery with: :exception
-  #include Error::ErrorHandler
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    respond(:record_not_found, 404, e.to_s)
+  end
+  rescue_from ActiveRecord::ActiveRecordError do |e|
+    respond(e.error, 422, e.to_s)
+  end
+  #rescue_from ActiveModel::ValidationError do |e|
+   # respond(e.error, 422, e.to_s)
+  #end
+  #rescue_from CustomApiError do |e|
+  #  respond(e.error, e.status, e.message.to_s)
+  #end
+  rescue_from CanCan::AccessDenied do
+    respond(:forbidden, 401, "current user isn't authorized for that")
+  end
+  rescue_from StandardError do |e|
+    respond(:standard_error, 500, e.to_s)
+  end
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
     end
 
-    # protect_from_forgery prepend: true
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  # protect_from_forgery prepend: true
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
     protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name, :surname, :birth_date, :username])
       devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
     end
+    def respond(_error, _status, _message)
+      json = Helpers::Render.json(_error, _status, _message)
+      render json: json
+    end
+    
 end
