d283fb4a1979c16b9bcf3a027927344b34afe834
tiziano finizzi <finizzi.1915610@studenti.uniroma1.it>
2022-09-07 16:48:55 +0200

    author_rating model test end

--- authormetrics/app/models/author_rating.rb
+++ authormetrics/app/models/author_rating.rb
@@ -3,5 +3,7 @@ class AuthorRating < ApplicationRecord
   belongs_to :user
 
   validates :rating, :presence=>true, numericality: { only_integer: true, less_than_or_equal_to: 10, greater_than_or_equal_to: 1 }
+  validates :author_id, :presence=>true, length: {minimum:12,maximum: 12}
+  validates :user_id, :presence=>true
 
 end
--- authormetrics/spec/models/author_rating_spec.rb
+++ authormetrics/spec/models/author_rating_spec.rb
@@ -42,8 +42,12 @@ RSpec.describe AuthorRating, type: :model do
   it "good format rating" do
     r=AuthorRating.new
     r.rating=6
-    r.user=User.new
-    r.author=Author.new
+    r.user=User.create(:user_id=>2,:name=>"marco",:username=>"mrcrvl",:surname=>"ruvolo",:birth_date=>"2000-05-02",:email=>"mrccrvl@gmail.com",:password=>"aaaaaaaaaaaa")
+    r.author=Author.create(author_id: "wT4V7isAAAAJ",                                         
+      name: "Mike Wingfield",                                            
+      affiliations: "Professor, Forestry and Agricultural Biotechnology Institute (FABI), University of Pretoria",
+      interests:"[{:title=>\"forest protection\", :serpapi_link=>\"https://serpapi.com/search.json?engine=google_scholar_profiles&hl=en&mauthors=label%3Aforest_protection\", :link=>\"https://scholar.google.com/citations?hl=en&view_op=search_authors&mauthors=label:forest_protection\"}, {:title=>\"mycology\", :serpapi_link=>\"https://serpapi.com/search.json?engine=google_scholar_profiles&hl=en&mauthors=label%3Amycology\", :link=>\"https://scholar.google.com/citations?hl=en&view_op=search_authors&mauthors=label:mycology\"}, {:title=>\"entomology\", :serpapi_link=>\"https://serpapi.com/search.json?engine=google_scholar_profiles&hl=en&mauthors=label%3Aentomology\", :link=>\"https://scholar.google.com/citations?hl=en&view_op=search_authors&mauthors=label:entomology\"}, {:title=>\"biotechnology\", :serpapi_link=>\"https://serpapi.com/search.json?engine=google_scholar_profiles&hl=en&mauthors=label%3Abiotechnology\", :link=>\"https://scholar.google.com/citations?hl=en&view_op=search_authors&mauthors=label:biotechnology\"}]"
+    )
     expect(r).to be_valid
   end
   it "nil rating error" do
