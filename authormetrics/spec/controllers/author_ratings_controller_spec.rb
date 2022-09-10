58e089b9e83ba19f87312432a7404e4252e2ad91
tiziano finizzi <finizzi.1915610@studenti.uniroma1.it>
2022-09-10 16:11:46 +0200

    stub controller

--- authormetrics/spec/controllers/author_ratings_controller_spec.rb
+++ authormetrics/spec/controllers/author_ratings_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 require 'author_ratings_controller'
 require_relative "../support/devise"
 
+
+
 RSpec.describe AuthorRatingsController, type: :controller do
   #TEST 1  NEW/CREATE/SHOW 
   include Devise::Test::ControllerHelpers
@@ -9,6 +11,41 @@ RSpec.describe AuthorRatingsController, type: :controller do
   describe "TEST 1.1  AuthorRating: method NEW->create" do
 
     include Devise::Test::ControllerHelpers 
+
+    controller do
+      before_action :get_author_user
+      def new
+        @author_rating = @author.author_rating.build
+        @author_rating.user=@user
+        @author_rating.author=@author
+        authorize! :create, @author_rating, :message => "BEWARE: you are not authorized to create author ratings."
+      end
+      def create
+        @author_rating = @author.author_rating.build(author_rating_params)
+        @author_rating.user_id=current_user.id
+        authorize! :create, @author_rating,:message => "BEWARE: you are not authorized to create author ratings."
+        respond_to do |format|
+          if @author_rating.save
+            format.html { redirect_to author_author_ratings_path(@author), notice: "Author rating was successfully created." }
+            format.json { render :show, status: :created, location: @author_rating }
+          else
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: @author_rating.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+      private
+      def get_author_user
+        @author=Author.find(params[:author_id])
+        if current_user!=nil
+          @user=User.find(current_user.id)
+        end 
+      end
+      def author_rating_params
+        params.require(:author_rating).permit(:author_rating_id,:rating, :author_id, :user_id)
+      end
+    end
+
     #factory seed
     fixtures :users
     let(:user1) { users(:one) }
@@ -49,9 +86,25 @@ RSpec.describe AuthorRatingsController, type: :controller do
   describe "TEST 1.2 AuthorRating: method INDEX" do
 
     include Devise::Test::ControllerHelpers
+
+    controller do
+      before_action :get_author_user
+      def index
+        authorize! :index, AuthorRating, :message => "BEWARE: you are not authorized to index author ratings."
+        @author_ratings=@author.author_rating
+      end
+      private
+      def get_author_user
+        @author=Author.find(params[:author_id])
+        if current_user!=nil
+          @user=User.find(current_user.id)
+        end 
+      end
+      def author_rating_params
+        params.require(:author_rating).permit(:author_rating_id,:rating, :author_id, :user_id)
+      end
+    end
     #factory seed
-    
-    
     fixtures :users
     let(:user1) { users(:one) }
     fixtures :authors
@@ -60,6 +113,7 @@ RSpec.describe AuthorRatingsController, type: :controller do
     let(:rating1) {author_ratings{:one}}
     let(:rating2) {author_ratings{:two}}
     it "correct function Index for utente_loggato" do
+      
       sign_in user1
       expect{get :index, params: {:author_id=>author.author_id}}.not_to raise_error
       expect(assigns(:author_ratings))==[rating1,rating2]
